d5cd94844f4a1344ec9b58e21ba36d6b005549a0
xss7321246 <xss7321246sh@aliyun.com>
2016-08-02 21:15:31 +0800

    "new date"

---
+++ src/helloword1/HelloWord.java
@@ -0,0 +1,12 @@
+package helloword1;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("你好世界，我是外星人");
+		System.out.println("我是异性，我想吸引你，然后把你吃掉");
+		System.out.println("大家好，我是铁血战士，我要把楼上干掉！");
+		Object a;
+		a=true?1:2.0;
+		System.out.println(a);
+	}
+}
